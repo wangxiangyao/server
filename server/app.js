400893f543f74c7d09de3b968bdd634c1db4de3e
wxy <349152111@qq.com>
2017-07-11 11:12:49 +0800

    简单服务器功能已经实现

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
 const http = require('http').createServer(app.callback());
-const roomServer = require('./roomServer');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2000;
 const controller = require('./controller');
 // 前端的接口，全都在controllers文件夹下的文件中定义，也就是路由。通过controller.js装载路由
 const bodyParser = require('koa-bodyparser');
@@ -20,9 +19,6 @@ app.use(staticFiles('/static/', path.join(__dirname, './../static')));
 // 部署路由
 console.log('开始部署路由');
 app.use(controller());
-
-// 启动socket.io
-roomServer(http);
 
 http.listen(port, function() {
 	console.log('Server listening at port %d', port);
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,21 @@
 var fs = require('fs');
 var path = require('path');
 
-var _staticDir = path.resolve('./views');
+var _staticDir = path.resolve('./data');
 
-var fn_index = async(ctx, next) => {
-	// 返回index.html
-	var indexPath = path.join(_staticDir, '/index.html');
+var fn_orders = async(ctx, next) => {
+	// 返回orders.json
+	var indexPath = path.join(_staticDir, '/orders.json');
 	ctx.response.body = fs.readFileSync(indexPath, 'utf-8');
+};
+var fn_goods = async(ctx, next) => {
+  // 返回goods.json
+  var indexPath = path.join(_staticDir, '/goods.json');
+  ctx.response.body = fs.readFileSync(indexPath, 'utf-8');
 };
 
 
 module.exports = {
-	'GET /': fn_index,
+	'GET /getOrders': fn_orders,
+  'GET /getGoods': fn_goods,
 }
